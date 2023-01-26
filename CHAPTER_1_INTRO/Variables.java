5747cff9f573b040af36e56365fae892a9a38afa
harsuuu <harshjoshi7895@gmail.com>
2023-01-26 20:13:55 +0530

    Variables

---
+++ CHAPTER_1_INTRO/Variables.java
@@ -0,0 +1,18 @@
+public class Variables 
+{
+    public static void main(String[] arg)
+    {
+           byte b=5;
+           int i=10;
+           float f=25.4f;
+           char a ='A';
+
+           System.out.println(b);
+           b=15;
+           System.out.println(b);
+           System.out.println(i);
+           System.out.println(f);
+           System.out.println(a);
+           
+    }   
+}
---
+++ CHAPTER_1_INTRO/Variables_2.java
@@ -0,0 +1,23 @@
+public class Variables_2
+{
+    public static void main(String[] arg)
+    {
+        int amount=100;
+        int Amount=200;
+        int amounT=300;
+        int marks$Student=99;
+        int rollNumber=32;   //camel cases.
+        // Variable2 name ka koi variable nhi bna skte qki bo hmari class ka bhi name hai.
+        float avgMarksOfClass =35.5f;   //camel cases
+        int _z=10;
+
+        System.out.println(amount);
+        System.out.println(Amount);
+        System.out.println(amounT);
+        System.out.println(marks$Student);
+        System.out.println(rollNumber);
+        System.out.println(avgMarksOfClass);
+        System.out.println(_z);
+
+    }    
+}
