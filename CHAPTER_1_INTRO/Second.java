e8f775095e1215d7a9730807e1813e59b976a02a
harsuuu <harshjoshi7895@gmail.com>
2023-02-09 14:28:57 +0530

    Basic addition in java

---
+++ CHAPTER_1_INTRO/Second.java
@@ -0,0 +1,12 @@
+class Second
+{
+    public static void main( String args[])
+    {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+
+        System.out.println("Addition is :" +c);
+    }
+}
