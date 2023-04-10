fa8cc1b6ae6618d6eb2d268925193b2232b32349
harsuuu <harshjoshi7895@gmail.com>
2023-04-10 16:50:48 +0530

    Check entered number interger or not ?

---
+++ CHAPTER_1_INTRO/Checker.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Checker {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        System.out.println("Enter the nums :");
+
+        boolean b1 =s.hasNextInt();
+
+        System.out.println(b1);
+    }
+}
