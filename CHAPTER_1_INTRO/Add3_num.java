3abdcafa0253a86d93b41a28bb72eeda6c2f70c0
harsuuu <harshjoshi7895@gmail.com>
2023-04-10 16:51:46 +0530

    Add Three numbers

---
+++ CHAPTER_1_INTRO/Add3_num.java
@@ -0,0 +1,10 @@
+public class Add3_num {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c=30;
+        int sum=a+b+c;
+        System.out.println("Sum of three number is : " +sum);
+    }
+}
