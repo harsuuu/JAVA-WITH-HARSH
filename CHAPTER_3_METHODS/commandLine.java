9c87d73645d5974622d6bad8ee11bbb5bbefc875
harsuuu <harshjoshi7895@gmail.com>
2023-04-17 22:37:46 +0530

    Command line Argument

---
+++ CHAPTER_3_METHODS/commandLine.java
@@ -0,0 +1,10 @@
+import javax.xml.transform.Source;
+
+public class commandLine {
+    public static void main(String[] args) {
+        for(String s: args)
+        {
+            System.out.println(s);
+        }
+    }
+}
