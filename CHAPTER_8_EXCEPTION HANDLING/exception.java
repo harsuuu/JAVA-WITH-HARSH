891e84c2a4ccfc2e3c1e7e3604fb803f4fce168f
harsuuu <harshjoshi7895@gmail.com>
2023-04-25 01:46:41 +0530

    basic

---
+++ CHAPTER_8_EXCEPTION HANDLING/exception.java
@@ -0,0 +1,12 @@
+public class exception {
+    public static void main(String[] args) {
+            int a,b,c;
+            a=6;
+            b=0;
+            c=a/b;
+            System.out.println(c);  // ye error hai to koi line execute nhi hogi.
+            System.out.println("Good Bye");
+            System.out.println("Good Morning");
+        
+    }
+}
